baa3b53adf6c66100e0307eec538d6cbc3df2f9c
romanmusiyowskij248@gmail.com <Igor130498>
2020-02-28 19:04:32 +0100

    Added market-service module

---
+++ market-service/src/test/java/com/mic/marketservice/MarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mic.marketservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
